677e538b9767455c22313297cfa81b2f01c1de78
Kul Karki <Kkarki349@rku.ac.in>
2024-04-02 23:18:16 +0530

    reigster updated

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "../styles/navbar.css";
+import { HashLink } from "react-router-hash-link";
+import { useDispatch } from "react-redux";
+import { setUserInfo } from "../redux/reducers/rootSlice";
+import { FiMenu } from "react-icons/fi";
+import { RxCross1 } from "react-icons/rx";
+import jwt_decode from "jwt-decode";
+import fetchData from "../helper/apiCall";
+
+const Navbar = () => {
+  const [iconActive, setIconActive] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
+  const [user, setUser] = useState(
+    localStorage.getItem("token")
+      ? jwt_decode(localStorage.getItem("token"))
+      : ""
+  );
+  const [isAdmin, setIsAdmin] = useState()
+
+  useEffect(() => {
+    const fetchAllDocs = async () => {
+      const data = await fetchData(`/user/userIsAdmin/${user.userId}`);
+      console.log("data ", data);
+    };
+    fetchAllDocs();
+  }, []);
+
+  const logoutFunc = () => {
+    dispatch(setUserInfo({}));
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+  console.log("token", token)
+  console.log("isAdmin", isAdmin);
+  return (
+    <header>
+      <nav className={iconActive ? "nav-active" : ""}>
+        <h2 className="nav-logo">
+          <NavLink to={"/"}>HealthBooker</NavLink>
+        </h2>
+        <ul className="nav-links">
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/doctors"}>Doctors</NavLink>
+          </li>
+          {token && user.isAdmin && (
+            <>
+            <li>
+              <NavLink to={"/dashboard/users"}>Dashboard</NavLink>
+            </li>
+            <li>
+            <NavLink to={"/applyfordoctor"}>Apply for doctor</NavLink>
+          </li></>
+          )}
+          {token && !user.isAdmin && (
+            <>
+              <li>
+                <NavLink to={"/appointments"}>Appointments</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/notifications"}>Notifications</NavLink>
+              </li>
+              
+              <li>
+                <HashLink to={"/#contact"}>Contact Us</HashLink>
+              </li>
+              <li>
+                <NavLink to={"/profile"}>Profile</NavLink>
+              </li>
+            </>
+          )}
+          {!token ? (
+            <>
+              <li>
+                <NavLink className="btn" to={"/login"}>
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="btn" to={"/register"}>
+                  Register
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <li>
+              <span className="btn" onClick={logoutFunc}>
+                Logout
+              </span>
+            </li>
+          )}
+        </ul>
+      </nav>
+      <div className="menu-icons">
+        {!iconActive && (
+          <FiMenu
+            className="menu-open"
+            onClick={() => {
+              setIconActive(true);
+            }}
+          />
+        )}
+        {iconActive && (
+          <RxCross1
+            className="menu-close"
+            onClick={() => {
+              setIconActive(false);
+            }}
+          />
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
