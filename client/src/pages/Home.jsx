f79ce7af88b3a2e6d8d90096416f8a86652dae61
Kul Karki <Kkarki349@rku.ac.in>
2024-04-02 23:28:10 +0530

    home updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import HomeCircles from "../components/HomeCircles";
-
+         
 const Home = () => {
   return (
     <>
